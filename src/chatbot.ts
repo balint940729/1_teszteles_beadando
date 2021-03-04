099ed9b04a203700b2d4bb8e1ba8bc7d710d96a9
Bálint Köles <koles.balint@gmail.com>
2021-03-04 15:28:47 +0100

    green: test2

--- src/chatbot.ts
+++ src/chatbot.ts
@@ -2,6 +2,9 @@ export class ChatBot{
     constructor(){}
 
     public greet(names: string[]): string{
+        if (names.length < 1) {
+            return "Hello, my friend.";
+        }
         return "Hello, " + names + ".";
     }
 }
