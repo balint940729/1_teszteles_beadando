2c6ecc7521015c76eae2c39fb3c1a583a68c40c8
Bálint Köles <koles.balint@gmail.com>
2021-03-04 20:11:23 +0100

    red: test Shout to single person

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -18,3 +18,13 @@ describe('Chatbot greet people', function () {
     expect(chatBot.greet(['Bob', 'Alex', 'Henry'])).toEqual("Hello, Bob, Alex, and Henry.");
   });
 });
+describe('Chatbot SHOUT to the people', function () {
+  let chatBot: ChatBot;
+  beforeEach(()=>{
+    chatBot = new ChatBot();
+  })
+
+  it('should SHOUT to one person', function () {
+    expect(chatBot.greet(['BOB'])).toEqual("HELLO BOB!");
+  });
+});
