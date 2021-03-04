5b450bc98423d613a707089c301b01fae66f45d1
Bálint Köles <koles.balint@gmail.com>
2021-03-04 15:17:32 +0100

    red: tes2

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -1,10 +1,17 @@
 import {ChatBot} from './chatbot';
 
 describe('Chatbot greet people', function () {
+  let chatBot: ChatBot;
+  beforeEach(()=>{
+    chatBot = new ChatBot();
+  })
 
   it('should greet the person', function () {
-    let chatBot = new ChatBot();
     expect(chatBot.greet(['Bob'])).toEqual("Hello, Bob.");
+  });
+
+  it('should greet the person without name', function () {
+    expect(chatBot.greet([])).toEqual("Hello, my friend.");
   });
 
 });
