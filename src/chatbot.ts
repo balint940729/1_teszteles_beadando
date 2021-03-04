a5888a916f36fffb74ac84efd25600cb93728de9
Bálint Köles <koles.balint@gmail.com>
2021-03-04 16:50:25 +0100

    green: test3

--- src/chatbot.ts
+++ src/chatbot.ts
@@ -2,8 +2,19 @@ export class ChatBot{
     constructor(){}
 
     public greet(names: string[]): string{
+        let sentence:string;
         if (names.length < 1) {
             return "Hello, my friend.";
+        }
+        if (names.length > 1) {
+            let i:number = 0;
+            sentence = "Hello, ";
+            while (i<names.length-1) {
+                sentence += names[i] + ", ";
+                i++;
+            }
+            sentence += "and " + names[names.length-1] + ".";
+            return sentence;
         }
         return "Hello, " + names + ".";
     }
