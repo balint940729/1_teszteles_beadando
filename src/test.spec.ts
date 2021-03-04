24486affb19e7921dafbb9fc8d91887ee46d50ea
Bálint Köles <koles.balint@gmail.com>
2021-03-04 16:37:05 +0100

    red: test3

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -14,4 +14,7 @@ describe('Chatbot greet people', function () {
     expect(chatBot.greet([])).toEqual("Hello, my friend.");
   });
 
+  it('should greet the persons with names and closes with and', function () {
+    expect(chatBot.greet(['Bob', 'Alex', 'Henry'])).toEqual("Hello, Bob, Alex, and Henry.");
+  });
 });
