8b9d4409580033f4583bce5dd8fadc1622ecf482
Bálint Köles <koles.balint@gmail.com>
2021-03-04 20:41:19 +0100

    red: Shout to multiple people in mixed input

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -14,10 +14,12 @@ describe('Chatbot greet people', function () {
     expect(chatBot.greet([])).toEqual("Hello, my friend.");
   });
 
-  it('should greet the persons with names and closes with and', function () {
+  it('should greet the peoples with names and closes with and', function () {
     expect(chatBot.greet(['Bob', 'Alex', 'Henry'])).toEqual("Hello, Bob, Alex, and Henry.");
   });
+
 });
+
 describe('Chatbot SHOUT to the people', function () {
   let chatBot: ChatBot;
   beforeAll(()=>{
@@ -26,5 +28,9 @@ describe('Chatbot SHOUT to the people', function () {
 
   it('should SHOUT to one person', function () {
     expect(chatBot.greet(['BOB'])).toEqual("HELLO BOB!");
+  });
+  
+  it('should greet or shout to multiple peoples', function () {
+    expect(chatBot.greet(['Bob', 'ALEX', 'Henry'])).toEqual("Hello, Bob, and Henry. HELLO ALEX!");
   });
 });
