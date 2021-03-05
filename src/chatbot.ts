0e3efff30b6be3dcd446941fd388b1e0e09588f8
Bálint Köles <koles.balint@gmail.com>
2021-03-05 13:24:40 +0100

    green: greet or shout to multiple people

--- src/chatbot.ts
+++ src/chatbot.ts
@@ -2,17 +2,11 @@ export class ChatBot{
     constructor(){}
 
     public greet(names: string[]): string{
-        let sentence:string;
-
-        if (names.length > 1) {
-            sentence = "Hello, ";
-            for(let i=0; i < names.length; i++){
-                if (i === names.length-1){
-                    return sentence += "and " + names[i] + ".";
-                }
-                sentence += names[i] + ", ";
-            }
-        }
+        let sentence:string = "Hello, ";
+        let sentence2:string = "HELLO ";
+        let upper: string[] = [];
+        let lower: string[] = [];
+        let both, shout, normal: boolean = false;
 
         if (names.length === 1) {
             if (names[0] === names[0].toUpperCase()) 
@@ -20,6 +14,66 @@ export class ChatBot{
             return "Hello, " + names + ".";
         }
 
+        if (names.length > 1){
+
+            // Szétbontom két csoportra a neveket nagy/kis-re
+            for(let i=0; i < names.length; i++){
+                if(names[i] === names[i].toUpperCase()){
+                    shout = true;
+                    upper.push(names[i]);
+                    continue;
+                }
+                normal = true;
+                lower.push(names[i]);
+            }
+
+            // Ha van legalabb 1-1 név mindkettőnel akkor
+            if (shout && normal) {
+                both = true;
+                sentence2 = " AND HELLO ";
+            }
+
+            if (normal && lower.length > 1){
+                for(let i=0; i < lower.length; i++){
+                    if(i === lower.length-1){
+                        sentence += "and " + lower[i] + ".";
+                        break;
+                    }
+                    sentence += lower[i] + ", ";
+                }
+            }
+
+            if (normal && lower.length === 1){
+                sentence += lower + ".";
+            }
+
+            if (shout && upper.length > 1){
+                for(let i=0; i < upper.length; i++){
+                    if(i === upper.length-1){
+                        sentence2 += "AND " + upper[i] + "!";
+                        break;
+                    }
+                    sentence2 += upper[i] + ", ";
+                }
+            }
+
+            if (shout && upper.length === 1){
+                sentence2 += upper + "!";
+            }
+
+            if (both){
+                return sentence + sentence2;
+            }
+
+            if (shout && !normal){
+                return sentence2;
+            }
+
+            return sentence;
+
+        }
+
         return "Hello, my friend.";
     }
+
 }
